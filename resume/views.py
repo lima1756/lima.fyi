f5dae66d997836e1db140cb106b9aa08053083d0
Iván Morett <luisivanmorett@gmail.com>
2017-12-15 19:40:20 -0600

    0.0.9b - Bootstrap :D
    Hey guess what, I decide to change Foundation to Bootstrap. "Why?" You probably would ask, simple, I think bootstrap is easy to use and gives the same options as Foundation, but at the start of the project I thought Foundation was simpler.

--- resume/views.py
+++ resume/views.py
@@ -1,3 +1,5 @@
+import json
+
 from os import path
 from ivanmorett.settings import STATICFILES_DIRS
 from django.shortcuts import render
@@ -6,9 +8,10 @@ from django.template import loader
 
 
 def index(request):
-    json = open(path.join(STATICFILES_DIRS[0], 'me.json'), encoding='utf-8').read()
-    print(json)
+    data = open(path.join(STATICFILES_DIRS[0], 'me.json'), encoding='utf-8').read()
+    decoded_json = json.loads(data)
+    print(decoded_json['personal_data'])
     context = {
-        'number': 5,
+        'data': decoded_json,
     }
     return render(request, 'resume/index.html', context)
