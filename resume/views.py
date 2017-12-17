ec68069d18f2767c258d47695c1003a14346482a
Iván Morett <luisivanmorett@gmail.com>
2017-12-17 13:05:34 -0600

    0.1.5 - The resume is in general complete
    The main page is complete, it takes all the required information from the json file and shows it to the user. I will make some future modifications, but right now I will continue with part of the Admin app.

--- resume/views.py
+++ resume/views.py
@@ -10,8 +10,19 @@ from django.template import loader
 def index(request):
     data = open(path.join(STATICFILES_DIRS[0], 'me.json'), encoding='utf-8').read()
     decoded_json = json.loads(data)
-    print(decoded_json['personal_data'])
+    aptitudes_categories = []
+    for apt in decoded_json['aptitudes']:
+        if apt['type'] not in aptitudes_categories:
+            aptitudes_categories.append(apt['type'])
+    print(aptitudes_categories)
+    aptitudes_categories_grid = 12//len(aptitudes_categories)
+    aptitudes_categories_grid = aptitudes_categories_grid if aptitudes_categories_grid >= 3 else 3
+    awards_grid = 12//len(decoded_json['awards'])
+    awards_grid = awards_grid if awards_grid >= 4 else 4
     context = {
         'data': decoded_json,
+        'aptitudes_categories': aptitudes_categories,
+        'aptitudes_categories_grid': aptitudes_categories_grid,
+        'awards_grid': awards_grid
     }
     return render(request, 'resume/index.html', context)
